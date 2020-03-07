4573c5a7fec60addc94aa40b8f2286452d901e60
Paolo Sciarra <itbhps@gmail.com>
2020-03-07 19:36:58 +0100

    trying out hot observable

--- src/test/java/it/twinsbrains/reactive/rxjava/ObservableTest.java
+++ src/test/java/it/twinsbrains/reactive/rxjava/ObservableTest.java
@@ -1,15 +1,20 @@
 package it.twinsbrains.reactive.rxjava;
 
 import io.reactivex.*;
+import io.reactivex.Observable;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.observables.ConnectableObservable;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
-import java.util.List;
+import java.time.Duration;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
 
 public class ObservableTest
 {
@@ -53,5 +58,52 @@ public class ObservableTest
     assertEquals(objects, asList("ciccio"));
   }
 
+  @Test
+  void hotObservableHangingInfinitely()
+  {
+    assertTimeout(Duration.ofSeconds(5), () -> {
+      Observable<String> strings = Observable
+          .just("Alpha", "Beta", "Gamma", "Delta", "Epsilon")
+          .publish();
+
+      strings.subscribe(System.out::println);
+    });
+  }
+
+  @Test
+  void hotObservableMultiCast()
+  {
+    List<String> subscriber1 = new LinkedList<>();
+    List<String> subscriber2 = new LinkedList<>();
+
+    ConnectableObservable<String> strings = Observable
+        .just("Alpha", "Beta", "Gamma", "Delta", "Epsilon")
+        .publish();
+
+    strings.subscribe(subscriber1::add);
+    strings.subscribe(subscriber2::add);
+    strings.connect();
+
+    assertEquals(subscriber1, asList("Alpha", "Beta", "Gamma", "Delta", "Epsilon"));
+    assertEquals(subscriber2, asList("Alpha", "Beta", "Gamma", "Delta", "Epsilon"));
+
+  }
+
+  @Test
+  void hotObservableLateSubscriberGetsNothing()
+  {
+    List<String> subscriber3 = new LinkedList<>();
+
+    ConnectableObservable<String> strings = Observable
+        .just("Alpha", "Beta", "Gamma", "Delta", "Epsilon")
+        .publish();
+
+    strings.connect();
+
+    Observable<List<String>> firstTwo = strings.buffer(2);
+    firstTwo.subscribe(subscriber3::addAll);
+
+    assertEquals(subscriber3, Collections.emptyList());
+  }
 
 }
